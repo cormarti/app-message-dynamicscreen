6fb6b319132f5558f4ec13264c77966be3c04da9
Corentin Martin <corentin.martin@dynamicscreen.com>
2021-09-29 10:09:16 +0200

    webhook test

--- src/simple-message/slide/SimpleMessage.ts
+++ src/simple-message/slide/SimpleMessage.ts
@@ -29,7 +29,7 @@ export default class SimpleMessageSlideModule extends SlideModule {
   };
 
   onMounted() {
-    console.log('sync onMounted webhook')
+    console.log('sync onMounted AGA webhook')
   }
 
   //@ts-ignore
