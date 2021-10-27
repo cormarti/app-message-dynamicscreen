de68938d9d8653f873daf31cef1548e93d18fa99
Corentin Martin <corentin.martin@dynamicscreen.com>
2021-10-27 13:27:39 +0200

    i18n + handler

--- src/simple-message/SimpleMessage.php
+++ src/simple-message/SimpleMessage.php
@@ -3,17 +3,40 @@
 namespace Vendor\MyApp;
 
 
+use App\Domain\Module\Model\Module;
 use DynamicScreen\SdkPhp\Slide\SlideHandler;
 use DynamicScreen\SdkPhp\Slide\ISlide;
 
-class Message extends SlideHandler {
+class Message extends SlideHandler
+{
+    private Module $module;
+
+    public function __construct(Module $module)
+    {
+        $this->module = $module;
+    }
+
+    public function getName(): string
+    {
+        return $this->module->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->module->description;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->module->identifier;
+    }
 
     public function fetch(ISlide $slide): array
     {
         return [
-            'title' => $slide->getOption('title'),
-            'message' => $slide->getOption('message'),
-            'backgroundColor' => $slide->getOption('backgroundColor'),
+            'title' => $slide->getOption('title', $this->getDefaultOptions()['title']),
+            'message' => $slide->getOption('message', $this->getDefaultOptions()['message']),
+            'backgroundColor' => $slide->getOption('backgroundColor', $this->getDefaultOptions()['title']),
         ];
     }
 
